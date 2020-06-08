0da2b2439fa3d2ca4479aba2f728c51c93e5ed00
Dev <eoindurnin@gmail.com>
2020-06-08 17:06:58 +0200

    defaults further reduced

--- image_reduce.php
+++ image_reduce.php
@@ -1,34 +1,31 @@
 <?php 
 
+/* Main compression function, takes an image, destination and quality level  */
 function compress_image($src, $dest , $quality) 
-{
+{ 
 	$info = getimagesize($src);
-
+	/*check image format */
 	if ($info["mime"] == "image/jpeg" ) 
 	{
 		$image = imagecreatefromjpeg($src);
+		/*reduces the image to quality, an saves it to destination */
 		imagejpeg($image, $dest, $quality);
 	}
 	elseif ($info["mime"] == "image/png") 
 	{
-
-		 file_put_contents($dest, compress_png($src, $quality)); 
-		/* copy(compress_png($src, $quality), $dest); */
-		
+		/* pngs handled seperately by compress_png function, result saved to destination */
+	file_put_contents($dest, compress_png($src, $quality)); 
 	}
 	else{
 		echo "Image type not recognized . . .$src";
 	}
-
 }
 
 
+/*takes file path to png image,  and maximum quality value for compression */
 function compress_png($path_to_png_file, $max_quality )
-{
-    if (!file_exists($path_to_png_file)) {
-        throw new Exception("File does not exist: $path_to_png_file");
-    }
-    $min_quality = 60;
+{   
+    $min_quality = 25;
     $compressed_png_content = shell_exec("pngquant --quality=$min_quality-$max_quality - < ".escapeshellarg(    $path_to_png_file));
 
     if (!$compressed_png_content) {
@@ -38,7 +35,6 @@ function compress_png($path_to_png_file, $max_quality )
     return $compressed_png_content;
 }
 
-
 /* Loop through images folder to build array of file paths as strings */
 $fileSystemIterator = new FilesystemIterator('images');
 $entries = array();
@@ -46,28 +42,23 @@ foreach ($fileSystemIterator as $fileInfo){
     $entries[] = $fileInfo->getFilename();
 }
 
-
-
 /* for every file path in the entries array, find the image size and then set the compression amount and call compress function */
  foreach($entries as $image){ 
 
 	$info = getimagesize("images/$image");
-	var_dump($image, $info[0], $info[1]); 
+	/* images with a height or a width greater than those of a product image are reduced to 75% quality */
+	 if($info[0] > 800 || $info[1] > 800 ) {
 
-	if($info[0] > 800 || $info[1] > 800 ) {
+	 compress_image("images/$image", "build/$image", 75 ); 
 
-		 compress_image("images/$image", "build/$image", 85 ); 
+	} elseif( (strpos($image, 'default') !== false)) {
 
-	} elseif( $info[0] <= 800 && $info[1] <= 800) {
+		compress_image("images/$image", "build/$image", 25 ); 
 
-		 compress_image("images/$image", "build/$image", 60 ); 
-
-	} else {
-
-		 compress_image("images/$image", "build/$image", 100 );
-
-	}	
-	
+	} else{
+			/* Product images or smaller are reduced to 55% quality */
+			compress_image("images/$image", "build/$image", 55 ); 
+		}
  } 
 
  ?> 
