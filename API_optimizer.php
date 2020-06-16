d487a2d6a690e508eaf2e057d43a0041bcfb2c63
Dev <eoindurnin@gmail.com>
2020-06-16 16:35:08 +0200

    refactoring as an API

--- API_optimizer.php
+++ API_optimizer.php
@@ -1,64 +1,41 @@
 <?php 
 
-$file = $_GET['file'];
-
-
-//iterate through folder to get file paths 
-$fileSystemIterator = new FilesystemIterator('images');
-$entries = array();
-foreach ($fileSystemIterator as $fileInfo){
-$entries[] = $fileInfo->getFilename();
-}
-
-//for each file, get the image quality, then call compress function with starting value of quality depending on the image dimensions
-foreach($entries as $image){
-    $info = getimagesize("images/$image");
-
-    //this is the cmd for returning the 'quality' of an image, it is form the imagick library 
-                //identiy -format '%f:%Q' filename.jpg //    for single image files (for testing)
-    // will return the 'quality' of the jpg file 
-    $output = shell_exec("identify -format '%f: %Q' images/$image");
-    //just keep the actualy value for quality
+//gets file path from the request params
+$filepath = $_GET['filepath'];
+    $info = getimagesize("$filepath");
+    //folder and file names need to be formatted correctly so they can be used in the shell command below
+    $output = shell_exec("identify -format '%f: %Q' $filepath");
     $str = substr($output, -2); 
-    //turn it into a number for comparison operators
     $quality_test_value = (int) $str; 
 
-        //to start reductions at lower $maximum_compression_quality values based on the dimensions of the picture using switch
-    switch ($info) {
-        case $info[0] >= 1200 || $info[1] >= 1200:
-        //for the biggest images(over 1200) call compress and start with 100 as the maximum value for quality
-            compress_image("images/$image", "build/O-$quality_test_value-MQ-100-X-$info[0]-Y-$info[1]-$image", 100, $quality_test_value, $info); 
-        break;
-        //as the images get smaller, reduce the maximum value for quality that is sent to the compress function
-        case $info[0] > 800 || $info[1] > 800:
-            compress_image("images/$image", "build/O-$quality_test_value-MQ-90-X-$info[0]-Y-$info[1]-$image", 90, $quality_test_value, $info); 
-        break;
-        case $info[0] > 400 || $info[1] > 400:
-            compress_image("images/$image", "build/O-$quality_test_value-MQ-80-X-$info[0]-Y-$info[1]-$image", 80, $quality_test_value, $info); 
-        break;	
-        case $info[0] <= 400 && $info[1] <= 400:
-             compress_image("images/$image", "build/O-$quality_test_value-MQ-70-X-$info[0]-Y-$info[1]-$image", 70, $quality_test_value, $info); 
-        break;
-        default:
-             compress_image("images/$image", "build/O-$quality_test_value-MQ-85-X-$info[0]-Y-$info[1]-$image", 85, $quality_test_value, $info);  
-        break;			
-    }	
+switch ($info) {
+    case $info[0] >= 1200 || $info[1] >= 1200:     
+        compress_image("$filepath", "$filepath", 100, $quality_test_value, $info); 
+    break;
+    case $info[0] > 800 || $info[1] > 800:
+        compress_image("$filepath", "$filepath", 90, $quality_test_value, $info); 
+    break;
+    case $info[0] > 400 || $info[1] > 400:
+        compress_image("$filepath", "$filepath", 80, $quality_test_value, $info); 
+    break;	
+    case $info[0] <= 400 && $info[1] <= 400:
+        compress_image("$filepath", "$filepath", 70, $quality_test_value, $info); 
+    break;
+    default:
+        compress_image("$filepath", "$filepath", 85, $quality_test_value, $info);  
+    break;			
+}	
 
-
-    //to reduce all images simply based on the $quality_test_value, with max_compresion_value of 80%
-  /*  compress_image("images/$image", "build/O-$quality_test_value-MQ-100-X-$info[0]-Y-$info[1]-$image", 80, $quality_test_value, $info); */
-} 
 
 function compress_image($src, $dest, $max_compression_quality, $quality_test_value, $info ) 
 {     
 $increment = 3; 
-/*check image format */
+// check image format 
 if ($info["mime"] == "image/jpeg" ) 
-{
+{   
+    //create an image resource from filepath string
     $image = imagecreatefromjpeg($src);
-    
-    //compress the jpg by maximum compression minus multiples of the increment depending on quality_test_value
-    //the lower the quality_test_value the more quality will be retained (which means a higher value sent to the imagejpeg function)
+
     switch ($quality_test_value) {
         case $quality_test_value <=40:
             imagejpeg($image, $dest, ($max_compression_quality - (1*$increment)));
@@ -154,25 +131,17 @@ if ($info["mime"] == "image/jpeg" )
         break;
         }
 }  else {
-		echo "Image type not recognized . . .$src";
+		echo "Image not recognized . . . $src";
 	}
 } 
 
-//this is the function for compressing png files 
 function compress_png($path_to_png_file, $quality )
-{   
-    //min_quality value needs to be below any number passed into the compress function,
-    //this is tricky because small low quality pngs can have very low values for quality passed to his function from the switch statements above
-    //png quality is different to jpg, it is done by using a range of values (hence, min and max)
+{  
     $min_quality = 20;
     $compressed_png_content = shell_exec("pngquant --quality=$min_quality-$quality - < ".escapeshellarg( $path_to_png_file));
 
     if (!$compressed_png_content) {
         throw new Exception("Conversion to compressed PNG failed. Is pngquant 1.8+ installed on the server?");
     }
-
     return $compressed_png_content;
 }
-
-
-
